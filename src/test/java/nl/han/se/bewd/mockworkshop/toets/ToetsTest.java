3ae99cd0d8e8e3072e3c37a1b117728f0dd1804a
bartugs <117641579+bartugs@users.noreply.github.com>
2024-09-25 11:17:21 +0200

    workshop

--- src/test/java/nl/han/se/bewd/mockworkshop/toets/ToetsTest.java
+++ src/test/java/nl/han/se/bewd/mockworkshop/toets/ToetsTest.java
@@ -3,6 +3,10 @@ package nl.han.se.bewd.mockworkshop.toets;
 import nl.han.se.bewd.mockworkshop.resultaat.ToetsResultaatRegistratieDB;
 import nl.han.se.bewd.mockworkshop.student.Student;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
@@ -10,15 +14,22 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 class ToetsTest {
+    @Mock
+    Student mockStudent = mock(Student.class);
+    ToetsResultaatRegistratieDB mockDB = mock(ToetsResultaatRegistratieDB.class);
+
+    @InjectMocks
+    Toets sut = new Toets();
+
+//    sut.setDb(mockDB);
 
     @Test
     void opdracht10getToetsCijferVoorStudentGeeftToetsCijferVoorStudent() {
         // Arrange
-        Student mockStudent = mock(Student.class);
-        ToetsResultaatRegistratieDB mockDB = mock(ToetsResultaatRegistratieDB.class);
-        Toets sut = new Toets();
-        sut.setDb(mockDB);
+
+//
         when(mockDB.vraagResultatenOp(mockStudent)).thenReturn(List.of("8"));
 
         // Act
@@ -31,10 +42,6 @@ class ToetsTest {
     @Test
     void opdracht10getToetsCijferVoorStudentGeeftToetsCijferVoorStudent2() {
         // Arrange
-        Student mockStudent = mock(Student.class);
-        ToetsResultaatRegistratieDB mockDB = mock(ToetsResultaatRegistratieDB.class);
-        Toets sut = new Toets();
-        sut.setDb(mockDB);
         when(mockDB.vraagResultatenOp(mockStudent)).thenReturn(List.of("2"));
 
         // Act
@@ -47,11 +54,11 @@ class ToetsTest {
     @Test
     void opdracht10getToetsCijferVoorStudentGeeftToetsCijferVoorStudent3() {
         // Arrange
-        Student mockStudent = mock(Student.class);
-        ToetsResultaatRegistratieDB mockDB = mock(ToetsResultaatRegistratieDB.class);
-        Toets sut = new Toets();
-        sut.setDb(mockDB);
-        when(mockDB.vraagResultatenOp(mockStudent)).thenReturn(List.of("1","2","3"));
+//        Student mockStudent = mock(Student.class);
+//        ToetsResultaatRegistratieDB mockDB = mock(ToetsResultaatRegistratieDB.class);
+//        Toets sut = new Toets();
+//        sut.setDb(mockDB);
+        when(mockDB.vraagResultatenOp(mockStudent)).thenReturn(List.of("1", "2", "3"));
 
         // Act
         int result = sut.getToetsCijferVoorStudent(mockStudent);
@@ -63,11 +70,11 @@ class ToetsTest {
     @Test
     void opdracht10getToetsCijferVoorStudentGeeftToetsCijferVoorStudent4() {
         // Arrange
-        Student mockStudent = mock(Student.class);
-        ToetsResultaatRegistratieDB mockDB = mock(ToetsResultaatRegistratieDB.class);
-        Toets sut = new Toets();
-        sut.setDb(mockDB);
-        when(mockDB.vraagResultatenOp(mockStudent)).thenReturn(List.of("10","2","3"));
+//        Student mockStudent = mock(Student.class);
+//        ToetsResultaatRegistratieDB mockDB = mock(ToetsResultaatRegistratieDB.class);
+//        Toets sut = new Toets();
+//        sut.setDb(mockDB);
+        when(mockDB.vraagResultatenOp(mockStudent)).thenReturn(List.of("10", "2", "3"));
 
         // Act
         int result = sut.getToetsCijferVoorStudent(mockStudent);
--- src/test/java/nl/han/se/bewd/mockworkshop/vak/VakTest.java
+++ src/test/java/nl/han/se/bewd/mockworkshop/vak/VakTest.java
@@ -1,6 +1,8 @@
 package nl.han.se.bewd.mockworkshop.vak;
 
+import nl.han.se.bewd.mockworkshop.student.FoutiefStudentException;
 import nl.han.se.bewd.mockworkshop.student.Student;
+import nl.han.se.bewd.mockworkshop.toets.FakeToets;
 import nl.han.se.bewd.mockworkshop.toets.Summatief;
 import nl.han.se.bewd.mockworkshop.toets.Toets;
 import org.junit.jupiter.api.Test;
@@ -8,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class VakTest {
 
@@ -30,10 +34,11 @@ class VakTest {
         // Arrange
         Student testStudent = new Student();
 
-         Toets toets1 = new Toets();
-         toets1.studentMaaktToets(testStudent, 8);
-         Toets toets2 = new Toets();
-         toets2.studentMaaktToets(testStudent, 6);
+        FakeToets toets1 = new FakeToets();
+        toets1.setReturnValueForGetToetsCijferVoorStudent(8);
+
+        FakeToets toets2 = new FakeToets();
+        toets2.setReturnValueForGetToetsCijferVoorStudent(6);
 
         Vak vak = new Vak(List.of(toets1, toets2));
 
@@ -49,8 +54,8 @@ class VakTest {
         // Arrange
         Student studentNietGemaakt = new Student();
         Student studentWelGemaakt = new Student();
-        Toets toets1 = new Toets();
-        toets1.studentMaaktToets(studentWelGemaakt, 8);
+        Toets toets1 = mock(Toets.class);
+//        toets1.studentMaaktToets(studentWelGemaakt, 8);
         Vak vak = new Vak(List.of(toets1));
 
         // Act
@@ -66,7 +71,9 @@ class VakTest {
         Student student1 = new Student();
         Student student2 = new Student();
 
-        Toets toets1 = new Toets();
+        Toets toets1 = mock(Toets.class);
+        when(toets1.getToetsCijferVoorStudent(student1)).thenReturn(3);
+        when(toets1.getToetsCijferVoorStudent(student2)).thenReturn(10);
         toets1.studentMaaktToets(student1, 3);
         toets1.studentMaaktToets(student2, 10);
         Vak vak = new Vak(List.of(toets1));
@@ -87,7 +94,7 @@ class VakTest {
     public void opdracht8verwijderStudentUitAllToetsenVerwijdertStudentUitToets() {
         // Arrange
         Student student1 = new Student();
-        Toets toets1 = new Toets();
+        Toets toets1 = mock(Toets.class);
         toets1.studentMaaktToets(student1, 10);
         Vak vak = new Vak(List.of(toets1));
 
@@ -95,16 +102,16 @@ class VakTest {
         vak.verwijderStudentUitAlleToetsen(student1);
 
         // Assert
-        int toetsCijferVoorStudent = toets1.getToetsCijferVoorStudent(student1);
-        assertEquals(0, toetsCijferVoorStudent);
+//        int toetsCijferVoorStudent = toets1.getToetsCijferVoorStudent(student1);
+        verify(toets1).verwijderStudentResultaten(student1);
     }
 
     @Test
     public void opdracht9verwijderStudentGooitRTEBijNullStudent() {
         // Arrange
-        Toets toets1 = new Toets();
+        Toets toets1 = mock(Toets.class);
         Vak vak = new Vak(List.of(toets1));
-
+        doThrow(FoutiefStudentException.class).when(toets1).verwijderStudentResultaten(isNull());
         // Act and Assert
         assertThrows(
                 RuntimeException.class,
